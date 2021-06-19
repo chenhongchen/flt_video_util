0118225345711752afe3e5d54c4ac7102dfa2f04
chenhongchen <412130100@qq.com>
2021-06-19 10:35:37 +0800

    适配flutter2

--- lib/flt_video_util.dart
+++ lib/flt_video_util.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 class FltVideoUtil {
   static const MethodChannel _channel = const MethodChannel('flt_video_util');
 
-  static Future<bool> compressToMp4(String videoPath, String mp4Path,
+  static Future<bool?> compressToMp4(String videoPath, String mp4Path,
       {double? maxSize, int? bitRate}) async {
     if (videoPath.length <= 0 || mp4Path.length <= 0) {
       return false;
@@ -17,7 +17,7 @@ class FltVideoUtil {
     param['maxSize'] = maxSize ?? 960.0;
     param['bitRate'] = bitRate ?? 3000000;
 
-    final bool isSuccess = await _channel.invokeMethod('compressToMp4', param);
+    final bool? isSuccess = await _channel.invokeMethod('compressToMp4', param);
     return isSuccess;
   }
 
@@ -28,9 +28,9 @@ class FltVideoUtil {
     Map param = Map();
     param['videoPath'] = videoPath;
 
-    Map map = await _channel.invokeMethod('getVideoSize', param);
-    double width = map['width'] ?? 0;
-    double height = map['height'] ?? 0;
+    Map? map = await _channel.invokeMethod('getVideoSize', param);
+    double width = map?['width'] ?? 0;
+    double height = map?['height'] ?? 0;
     Size size = Size(width, height);
     return size;
   }
